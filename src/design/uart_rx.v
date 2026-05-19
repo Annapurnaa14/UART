be6b4204dcbd19ab820d4d95a125725730cd5be4
Annapurna Shenoy <168350449+Annapurnaa14@users.noreply.github.com>
2026-05-19 13:51:03 +0530

    Update uart_rx.v

--- src/design/uart_rx.v
+++ src/design/uart_rx.v
@@ -1,10 +1,11 @@
 module uart_rx #(parameter width = 8)(
-    input  sys_rst,
+    input sys_rst,
     input  baud_op_clk,
-    input  uart_rec_data_h,
-    output reg  rec_busy,
-    output reg  rec_ready,
-    output reg [width-1:0]  rec_data_h
+    input   uart_rec_data_h,
+    output reg    rec_busy,
+    output reg   rec_ready,
+    output reg [width-1:0]  rec_data_h,
+    output reg [3:0] count
 );
     localparam idle  = 2'd0,
                start = 2'd1,
@@ -12,61 +13,72 @@ module uart_rx #(parameter width = 8)(
                stop  = 2'd3;
 
     reg [1:0] ct, nt;
-    reg [3:0] count;
     reg [$clog2(width):0] index;
-    reg   rx1, rx2;
+    reg rx1, rx2;
+    reg rx2_sampled;
 
     
     always @(posedge baud_op_clk or negedge sys_rst) begin
-        if (!sys_rst)
-          begin rx1 <= 1'b1; rx2 <= 1'b1; end
-        else         
-          begin rx1 <= uart_rec_data_h; rx2 <= rx1; end
+        if (!sys_rst) begin rx1 <= 1'b1; rx2 <= 1'b1; end
+        else          begin rx1 <= uart_rec_data_h; rx2 <= rx1; end
     end
 
     always @(posedge baud_op_clk or negedge sys_rst) begin
         if (!sys_rst) begin
-            ct         <= idle;
-            count      <= 0;
-            index      <= 0;
-            rec_data_h <= 0;
-            rec_ready  <= 1;
-            rec_busy   <= 0;
+            ct          <= idle;
+            count       <= 0;
+            index       <= 0;
+            rec_data_h  <= 0;
+            rec_ready   <= 0;
+            rec_busy    <= 0;
+            rx2_sampled <= 1;
         end
         else begin
             ct <= nt;
 
-           
             if (ct != nt) count <= 0;
             else          count <= count + 1;
 
-           
-            if (ct == data && count == 7)
-                rec_data_h <= {rx2, rec_data_h[width-1:1]};  
+            
+            if (ct == data && count == 5)
+                rx2_sampled <= rx2;
 
            
+            if (ct == data && count == 6)
+                rec_data_h <= {rx2_sampled, rec_data_h[width-1:1]};
+
+            
             if (ct == idle)
                 index <= 0;
             else if (ct == data && count == 15)
                 index <= index + 1;
 
-            
-            if (ct == stop && count == 7 && rx2 != 1'b1)
+           
+            if (ct == stop && count == 15 && rx2 != 1'b1)
                 rec_data_h <= 0;
 
-            rec_ready <= (nt == idle);
-            rec_busy  <= (nt != idle);
+            
+            if (ct == stop && count == 15 && rx2 == 1'b1)
+                rec_ready <= 1;
+            else
+                rec_ready <= 0;
+
+            rec_busy <= (nt != idle);
         end
     end
 
     always @(*) begin
         nt = ct;
         case (ct)
-            idle:  nt = (rx2 == 1'b0) ? start : idle;
+            
+            idle: nt = (uart_rec_data_h == 1'b0) ? start : idle;
 
+            
             start: begin
-                if (count == 12)
-                    nt = (rx2 == 1'b0) ? data : idle;
+                if (count == 6 && rx2 != 1'b0)
+                    nt = idle;          
+                else if (count == 15)
+                    nt = data;          
                 else
                     nt = start;
             end
@@ -78,7 +90,8 @@ module uart_rx #(parameter width = 8)(
                     nt = data;
             end
 
-            stop:  nt = (count == 15) ? idle : stop;
+            stop: nt = (count == 15) ? idle : stop;
+
             default: nt = idle;
         endcase
     end
